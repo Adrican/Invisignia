14676f80cf5a9555aae260c7fbf1b6508239a77f
Adrian Canas <adriancan21@gmail.com>
2025-07-26 14:37:48 +0200

    Funcionando la marca de agua y las credenciales de base de datos movidas a .env

--- app/database.py
+++ app/database.py
@@ -1,19 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
-from fastapi import Depends
+from app.core.config import settings
 
-USER = "postgres"
-PASSWORD = "password"
-HOST = "localhost"
-PORT = 5432
-DB_NAME = "invisignia"
-
-DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
-
-engine = create_engine(DATABASE_URL)
-
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
